02e4a9dc56d71c63d06726a63fcbf32971515cdb
Shuye Pu <shuye2009@gmail.com>
2023-07-27 18:10:34 -0400

    edit data.R

--- R/data.R
+++ R/data.R
@@ -30,7 +30,7 @@
 #'      human gene names in different columns. 
 #' }
 #' @source The original gene annotation (gtf) file is downloaded from 
-#'      \url{https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_19/gencode.v19.annotation.gtf.gz}
+#'      \href{https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_19/gencode.v19.annotation.gtf.gz}{https://www.gencodegenes.org/human/}. \cr
 #'      Except for the gtf file, all other files are derived from experimental 
 #'      data produced in-house at the \href{https://thedonnellycentre.utoronto.ca/faculty/jack-greenblatt}{Greenblatt Lab, University of Toronto, Canada}.  
 #' @return Various files used as inputs to run examples and tests
@@ -50,27 +50,18 @@ NULL
 #' @title Toy data for examples and testing of the `GenomicPlot` package
 #' @description A tiny TxDb object holding genomic feature coordinates of 72 
 #' transcripts in hg19.
-#' @return 
-#' A SQLlite database  
-#' @source
-#' The data is produced by running the following code:
-#' \describe{
+#' @source The data is produced by running the following code: \cr
 #' gtffile <- system.file("extdata", "gencode.v19.annotation_chr19.gtf", 
-#' package = "GenomicPlot")
-#' }
-#' \describe{
+#' package = "GenomicPlot") \cr
 #' gff <- RCAS::importGtf(saveObjectAsRds = TRUE, readFromRds = FALSE, 
-#'     filePath = gtffile)
-#' }
-#' \describe{
-#' md <- data.frame(name = "Genome", value = "hg19")
-#' }
-#' \describe{
-#' txdb <- GenomicFeatures::makeTxDbFromGRanges(gff, metadata = md)
-#' }
-#' \describe{
+#'     filePath = gtffile) \cr
+#' md <- data.frame(name = "Genome", value = "hg19") \cr
+#' txdb <- GenomicFeatures::makeTxDbFromGRanges(gff, metadata = md) \cr
 #' AnnotationDbi::saveDb(txdb, "./inst/extdata/txdb.sql")
-#' }
+#' 
+#' @return 
+#' A SQLlite database 
+#' 
 #' @author Shuye Pu
 #' @docType data
 #' @name txdb.sql
@@ -84,6 +75,13 @@ NULL
 #' features promoter, 5'UTR, CDS, 3'UTR, TTS, as well as user inputs for
 #' processing these features.
 #' See \code{\link{prepare_5parts_genomic_features}} for details.
+#' 
+#' @source The data is produced by running the following code: \cr
+#' txdb <- AnnotationDbi::loadDb(system.file("extdata", "txdb.sql", 
+#'     package = "GenomicPlot")) \cr
+#' gf5_genomic <- GenomicPlot::prepare_5parts_genomic_features(txdb, 
+#'     meta = FALSE, nbins = 100, fiveP = -2000, threeP = 1000, longest = TRUE)
+#'     
 #' @return 
 #' A named list with the following elements:
 #' \describe{
@@ -95,16 +93,7 @@ NULL
 #'  \item{meta}{logical}
 #'  \item{longest}{logical}
 #' }
-#' @source
-#' The data is produced by running the following code:
-#' \describe{
-#' txdb <- AnnotationDbi::loadDb(system.file("extdata", "txdb.sql", 
-#'     package = "GenomicPlot"))
-#' }
-#' \describe{
-#' gf5_genomic <- GenomicPlot::prepare_5parts_genomic_features(txdb, 
-#'     meta = FALSE, nbins = 100, fiveP = -2000, threeP = 1000, longest = TRUE)
-#' }
+#' 
 #' @author Shuye Pu
 #' @docType data
 #' @name gf5_genomic
@@ -118,6 +107,14 @@ NULL
 #' features promoter, 5'UTR, CDS, 3'UTR, TTS, as well as user inputs for
 #' processing these features. 
 #' See \code{\link{prepare_5parts_genomic_features}} for details.
+#' 
+#' @source The data is produced by running the following code: \cr
+#' txdb <- AnnotationDbi::loadDb(system.file("extdata", "txdb.sql", 
+#'     package = "GenomicPlot")) \cr
+#' gf5_meta <- GenomicPlot::prepare_5parts_genomic_features(txdb, meta = TRUE, 
+#'     nbins = 100, fiveP = -2000, threeP = 1000, longest = TRUE)
+#' }
+#' 
 #' @return 
 #' A named list with the following elements:
 #' \describe{
@@ -128,16 +125,6 @@ NULL
 #'  \item{threeP}{a positive integer}
 #'  \item{meta}{logical}
 #'  \item{longest}{logical}
-#' }
-#' @source
-#' The data is produced by running the following code:
-#' \describe{
-#' txdb <- AnnotationDbi::loadDb(system.file("extdata", "txdb.sql", 
-#'     package = "GenomicPlot"))
-#' }
-#' \describe{
-#' gf5_meta <- GenomicPlot::prepare_5parts_genomic_features(txdb, meta = TRUE, 
-#'     nbins = 100, fiveP = -2000, threeP = 1000, longest = TRUE)
 #' }
 #' 
 #' @author Shuye Pu
